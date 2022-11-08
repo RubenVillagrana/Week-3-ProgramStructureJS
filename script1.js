8dd1ac55944b0d531ca5b39dcbbb3ee6d58402c9
Ruben Villagrana <RubenVillagrana35@gmail.com>
2022-11-07 21:01:11 -0800

    trying to loop

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 for (let line = "#"; line.length < 5; line += "#")
   console.log(line);
   
-  for (let line = "#"; line.length > 5; line += "#")
+  for (let line = "#"; line.length < 5 ; line -= "#")
   console.log(line);
