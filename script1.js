69a9ac3e719ab3a6ae45bf6c8031cc5078ab47d0
Ruben Villagrana <111696775+RubenVillagrana@users.noreply.github.com>
2022-11-08 21:52:41 -0800

    figured out my columns and rows

--- script1.js
+++ script1.js
@@ -1,5 +1,15 @@
-for (let line = "#"; line.length < 5; line += "#")
-  console.log(line);
-  
-  for (let line = "#"; line.length < 5 ; line -= "#")
-  console.log(line);
+let n = 4;
+let string = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    string += "#";
+  }
+  string += "\n";
+}
+for (let i = 1; i <= n - 1; i++) {
+  for (let j = 0; j < n - i; j++) {
+    string += "#";
+  }
+  string += "\n";
+}
+console.log(string);
