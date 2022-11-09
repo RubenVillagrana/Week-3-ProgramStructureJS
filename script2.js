e7bd722bd7e87975369a58e9ab1194296fc6d85f
Ruben Villagrana <111696775+RubenVillagrana@users.noreply.github.com>
2022-11-08 23:05:00 -0800

    Update script2.js

---
+++ script2.js
@@ -0,0 +1,29 @@
+      int n = 100
+      
+     for (int i=1; i<=n;
+     i++)
+     {
+     //number divisible by 15(divisible by
+     //both 3 & 5), print 'FizzBuzz' in
+     //place of the number
+     if 
+ (%15==0) 
+            System.out.println ("FizzBuzz");
+            //number divisble by 5, print'Buzz'
+            //in place of the number
+            else if (i%5==0)
+             System.out.println("Buzz")
+             
+             //number divisible by 3, print 'Fizz'
+             //in place of number
+             else if (i%3==0)
+             System.out.println("Fizz");
+             
+             else //print the numbers
+                System.out.println(i);
+       }
+    }
+ }
+    
+     
+     
